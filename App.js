2587ec2b876185e5d1eeb926b5fac8a7fcc3c15c
prathuziggletech <prathu@ziggletech.com>
2024-07-30 12:59:21 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,22 @@
+// const heading = React.createElement('h1', {
+//      id:'heading',
+//      className:'heading'
+// }, 'Hello from React!');
+
+// console.log(heading)
+
+const parent = React.createElement('div', { id: 'parent' },
+     [React.createElement('div', { id: 'child1' }, 
+          [React.createElement('h1', { id: 'heading' }, 'Heading1 from nested react'),
+               React.createElement('p', { id: 'para1' }, 'Paragraph1 from nested react')
+          ]),
+          React.createElement('div', { id: 'child2' }, 
+               [React.createElement('h1', { id: 'headin2' }, 'Heading2 from nested react'),
+                    React.createElement('p', { id: 'para2' }, 'Paragraph2 from nested react')
+               ]),
+     ]
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
